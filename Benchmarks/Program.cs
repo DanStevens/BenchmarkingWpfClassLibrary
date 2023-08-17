5b49b8d865619e8584135696a83ca94f22f5ab32
Dan Stevens <Daniel.STEVENS@oxinst.com>
2023-08-17 13:02:50 +0100

    Added code for benchmarking a WPF Class Library

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Benchmarks
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Jobs;
+    using BenchmarkDotNet.Running;
+    using WpfClassLibrary;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var results = BenchmarkRunner.Run<WpfClassBenchmarks>();
+        }
+    }
+
+    //[SimpleJob(RuntimeMoniker.Net48)]
+    //[SimpleJob(RuntimeMoniker.Net60)]
+    public class WpfClassBenchmarks
+    {
+        [Benchmark]
+        public void WpfClass()
+        {
+            _ = new WpfClass();
+        }
+    }
+}
---
+++ WpfClassLibrary/WpfClass.cs
@@ -0,0 +1,9 @@
+namespace WpfClassLibrary
+{
+    using System.Windows.Documents;
+
+    public class WpfClass
+    {
+        public FlowDocument Document { get; } = new FlowDocument();
+    }
+}
