664652ae98ab2c0a952b8b2efe3ee98d8bc80aae
Dan Stevens <Daniel.STEVENS@oxinst.com>
2024-10-03 10:19:53 +0100

    WIP

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace Benchmarks
 {
     using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Environments;
     using BenchmarkDotNet.Jobs;
     using BenchmarkDotNet.Running;
     using WpfClassLibrary;
@@ -9,7 +11,10 @@
     {
         static void Main(string[] args)
         {
-            var results = BenchmarkRunner.Run<WpfClassBenchmarks>();
+            var config = DefaultConfig.Instance
+                .AddJob(Job.Default.WithId("net60"))
+                .AddJob(Job.Default.WithRuntime(ClrRuntime.Net48).WithId("net48"));
+            var results = BenchmarkRunner.Run<WpfClassBenchmarks>(config);
         }
     }
 
